4107404ccff43c5fbaea3aaf62cf7255f8cff711
RohanG28 <rohan280502@gmail.com>
2022-06-17 16:31:43 -0700

    Added Header Divisions

---
+++ src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className="fixed z-50 w-screen bg-red-500 p-6">
+        <div className="hidden md:flex w-full h-full bg-slate-300 p-4">
+
+        </div>
+
+        <div className="flex md:hidden w-full h-full bg-purple-500 p-4">
+
+        </div>
+    </div>
+
+  )
+}
+
+export default Header
