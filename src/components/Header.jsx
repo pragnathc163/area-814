daac419b7bb3cd9d71a23f9822330c35bbd66709
pragnathc163 <prag2002@uw.edu>
2022-06-17 16:58:19 -0700

    Added custom CSS and logo to navBar

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import React from 'react';
+import Logo from './img/logo.png';
 
 const Header = () => {
   return (
-    <div className="fixed z-50 w-screen bg-red-500 p-6">
-        <div className="hidden md:flex w-full h-full bg-slate-300 p-4">
+    <div className="fixed z-50 w-screen p-6 px-16">
+      {/* Desktop Stuff */}
+      <div className="hidden md:flex w-full h-full"></div>
+      <div className='flex items-center gap-2'>
+        <img src={Logo} className="w-10 object-cover" alt='logo' />
 
-        </div>
-
-        <div className="flex md:hidden w-full h-full bg-purple-500 p-4">
-
-        </div>
+      </div>
+      {/* Mobile stuff */}
+      <div className="flex md:hidden w-full h-full"></div>
     </div>
 
   )
