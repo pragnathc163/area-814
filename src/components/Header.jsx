02117c5ef4b1591e0d4edae462a1ddfdbf807e69
pragnathc163 <prag2002@uw.edu>
2022-06-17 18:11:35 -0700

    Completed NavBar, working on Routes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,45 @@
 import React from 'react';
-import Logo from './img/logo.png';
+import { MdShoppingCart } from 'react-icons/md';
+
+import Logo from '../img/logo.png';
+import User from '../img/user.png';
 
 const Header = () => {
   return (
-    <div className="fixed z-50 w-screen p-6 px-16">
+    <header className="fixed z-50 w-screen p-6 px-16">
       {/* Desktop Stuff */}
-      <div className="hidden md:flex w-full h-full"></div>
-      <div className='flex items-center gap-2'>
-        <img src={Logo} className="w-10 object-cover" alt='logo' />
+      <div className="hidden md:flex w-full h-full items-center justify-between">
+        <div className='flex items-center gap-2'>
+          <img src={Logo} className="w-20 object-cover" alt='logo' />
+          <p className='text-headingColor text-x1 font-bold'>CycleFood</p>
+        </div>
+
+        <div className='flex items-center gap-8'>
+          <ul className='flex items-center gap-8'>
+            <li className='text-base text-textColor hover:text-headingColor duration-100
+          transition-all ease-in-out cursor-pointer'>Home</li>
+            <li className='text-base text-textColor hover:text-headingColor duration-100
+          transition-all ease-in-out cursor-pointer'>About Us</li>
+            <li className='text-base text-textColor hover:text-headingColor duration-100
+          transition-all ease-in-out cursor-pointer'>For You</li>
+          </ul>
+
+          <div className='relative flex items-center justify-center'>
+            <MdShoppingCart className='text-textColor text-2xl cursor-pointer' />
+            <div className='absolute top-3 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center'>
+              <p className='text-xs text-white font-semibold'>2</p>
+            </div>
+          </div>
+
+          <img src={User} className='w-10 min-w-[40px] h-10 min-h-[40px] drop-shadow-xl' alt='user profile' />
+
+        </div>
 
       </div>
+
       {/* Mobile stuff */}
       <div className="flex md:hidden w-full h-full"></div>
-    </div>
+    </header>
 
   )
 }
